d11992f815af0ec2dbae0a7f50438bc4104de338
Munib <munibrhmn@gmail.com>
2017-12-29 14:28:31 -0700

    Update Q5.hs

--- MT/Q5.hs
+++ MT/Q5.hs
@@ -2,8 +2,8 @@ data TriviaQuestion = NumericQ String Float | MCQ String String String String St
 
 instance Eq (TriviaQuestion) where
 
-	MCQ q _ _ _ _ _   == MCQ r _ _ _ _ _ = q == r
-	NumericQ q _      == NumericQ r _ _ = q == r
+    MCQ q _ _ _ _ _   == MCQ r _ _ _ _ _ = q == r
+    NumericQ q _      == NumericQ r _ _ = q == r
     (NumericQ x _)    == (MCQ y _ _ _ _ _) = x == y
     (MCQ y _ _ _ _ _) == (NumericQ x _)    = x == y
-    _ == _ = False
+                    _ == _ = False
